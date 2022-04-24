36dbb30c25b053a3bef113996b5f0d1720c9869d
Fiachra Barry <fiachra.barry@wayflyer.com>
2022-04-24 15:29:47 +0100

    added delete functionality + formatting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import { db } from './firebase-config';
-import { collection, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 function App() {
   const [newName, setName] = useState("");
@@ -11,50 +18,66 @@ function App() {
   const usersCollectionRef = collection(db, "users");
 
   const createUser = async () => {
-    await addDoc(usersCollectionRef, {name: newName, age: Number(newAge)});
-  }
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
 
-  const updateUser = async (id, age) =>{
-    const newFields = {age: age+1}
-  
-    await updateDoc(doc(db, "users", id), newFields)
-  }
+  const updateUser = async (id, age) => {
+    const newFields = { age: age + 1 };
+
+    await updateDoc(doc(db, "users", id), newFields);
+  };
+
+  const deleteUser = async (id) => {
+    await deleteDoc(doc(db, "users", id));
+  };
 
   useEffect(() => {
-
     const getUsers = async () => {
       const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getUsers();
-
-  }, [])
-
+  }, []);
 
   return (
     <div className="App">
-      <input 
-        placeholder='Name...'
-        onChange={(event => {setName(event.target.value)})}
+      <input
+        placeholder="Name..."
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
       />
-      <input 
-        type='number' 
-        placeholder='Age...'
-        onChange={(event => {setAge(event.target.value)})}
+      <input
+        type="number"
+        placeholder="Age..."
+        onChange={(event) => {
+          setAge(event.target.value);
+        }}
       />
-      <button onClick={ createUser }>Create User</button>
-      {
-        users.map((user) => {
-          return (
-            <div>
-              <h1>Name: {user.name}</h1>
-              <h1>Age: {user.age}</h1>
-              <button onClick={() => {updateUser(user.id, user.age)}}>Increase Age</button>
-            </div>
-          );
-        })
-      }
+      <button onClick={createUser}>Create User</button>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              Increase Age
+            </button>
+            <button
+              onClick={() => {
+                deleteUser(user.id);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
