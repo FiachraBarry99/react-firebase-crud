770c61be4281238f9db0425f681955e5bdf08747
Fiachra Barry <fiachra.barry@wayflyer.com>
2022-04-24 14:52:52 +0100

    added create and update functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { db } from './firebase-config';
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
 
 function App() {
+  const [newName, setName] = useState("");
+  const [newAge, setAge] = useState(0);
   const [users, setUsers] = useState([]);
 
   const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, {name: newName, age: Number(newAge)});
+  }
+
+  const updateUser = async (id, age) =>{
+    const newFields = {age: age+1}
+  
+    await updateDoc(doc(db, "users", id), newFields)
+  }
 
   useEffect(() => {
 
@@ -22,13 +34,23 @@ function App() {
 
   return (
     <div className="App">
-      <button>Create User</button>
+      <input 
+        placeholder='Name...'
+        onChange={(event => {setName(event.target.value)})}
+      />
+      <input 
+        type='number' 
+        placeholder='Age...'
+        onChange={(event => {setAge(event.target.value)})}
+      />
+      <button onClick={ createUser }>Create User</button>
       {
         users.map((user) => {
           return (
             <div>
               <h1>Name: {user.name}</h1>
               <h1>Age: {user.age}</h1>
+              <button onClick={() => {updateUser(user.id, user.age)}}>Increase Age</button>
             </div>
           );
         })
